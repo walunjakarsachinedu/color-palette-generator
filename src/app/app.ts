83eae40339e3ceb3c4507fab4080c265df88a221
Sachin Walunjakar <walunjakarsachin@gmail.com>
2025-12-14 15:04:57 +0530

    added option to copy colors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dialog } from './dialog/dialog';
+import { ColorPalette } from './dialog/model';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dialog],
+  imports: [RouterOutlet, Dialog, NgTemplateOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('color-palette');
-  protected readonly colorPalette = signal<{ variation: string, color: string }[]>([
+  protected readonly colorPalette = signal<ColorPalette[]>([
     { color: 'var(--brand-color)', variation: "brand color" },
     { color: 'var(--text-strong)', variation: "text strong" },
     { color: 'var(--text-weak)', variation: "text weak" },
@@ -21,6 +23,10 @@ export class App {
   
   protected readonly mode = signal<'light' | 'dark'>("dark");
   protected readonly brandColor = signal<string>('#4da3ff'); // Default Blue
+
+  ngOnInit(): void {
+    this.refreshPaletteValues();
+  }
 
   toggleTheme() {
     const theme = this.mode() === "dark" ? "light" : "dark";
@@ -42,6 +48,30 @@ export class App {
     // 2. Convert Hex to Hue (0-360) and update the Theme Engine
     const hue = this.getHueFromHex(hexValue);
     document.documentElement.style.setProperty('--brand-color-hue', hue.toString());
+
+    // 3. Resolve the actual values
+    this.refreshPaletteValues();
+  }
+
+  copyColor(color: string, index: number) {
+    // 1. Copy to clipboard
+    navigator.clipboard.writeText(color);
+
+    // 2. Set 'isCopied' to true for THIS item only
+    this.colorPalette.update(items => {
+      const newItems = [...items]; // Create a shallow copy
+      newItems[index] = { ...newItems[index], isCopied: true };
+      return newItems;
+    });
+
+    // 3. Reset after 1 second (1000ms)
+    setTimeout(() => {
+      this.colorPalette.update(items => {
+        const newItems = [...items];
+        newItems[index] = { ...newItems[index], isCopied: false };
+        return newItems;
+      });
+    }, 1000);
   }
 
   private getHueFromHex(hex: string): number {
@@ -65,5 +95,44 @@ export class App {
     if (h < 0) h += 360;
 
     return h;
+  }
+
+  private refreshPaletteValues() {
+    this.colorPalette.update(palette => {
+      return palette.map(item => ({
+        ...item,
+        resolvedValue: this.readCssVar(item.color)
+      }));
+    });
+  }
+
+  // TODO: optimize it to calculate color value without creating dom element 
+  private readCssVar(varUsage: string): string {
+    const tempEl = document.createElement('div');
+
+    tempEl.style.backgroundColor = varUsage;
+    tempEl.style.display = 'none'; 
+    document.body.appendChild(tempEl);
+
+    const rgbString = getComputedStyle(tempEl).backgroundColor;
+    
+    document.body.removeChild(tempEl);
+
+    return this.rgbToHex(rgbString);
+  }
+
+  /** Helper to convert "rgb(r, g, b)" -> "#rrggbb" */
+  private rgbToHex(rgb: string): string {
+    // Extract the numbers from the string
+    const result = rgb.match(/\d+/g);
+    
+    if (!result || result.length < 3) return rgb; 
+
+    // Convert each channel to Hex and pad with zero if needed
+    const r = parseInt(result[0]).toString(16).padStart(2, '0');
+    const g = parseInt(result[1]).toString(16).padStart(2, '0');
+    const b = parseInt(result[2]).toString(16).padStart(2, '0');
+
+    return `#${r}${g}${b}`;
   }
 }
--- src/app/dialog/model.ts
+++ src/app/dialog/model.ts
@@ -1,1 +1,13 @@
-export type User = { name: string, email: string, img: string, tag?: string };
+export type User = { 
+  name: string, 
+  email: string, 
+  img: string, 
+  tag?: string 
+};
+
+export type ColorPalette = { 
+  variation: string, 
+  color: string, 
+  resolvedValue?: string 
+  isCopied?: boolean
+}
